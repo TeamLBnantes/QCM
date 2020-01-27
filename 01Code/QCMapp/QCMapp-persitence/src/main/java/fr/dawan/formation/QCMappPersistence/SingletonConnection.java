a81d9ee239d8f46e36aa85dcab51a4725ab984ca
Vincent <billard.vincent@gmail.com>
2020-01-27 09:23:07 +0100

    taff WE 27-01

---
+++ 01Code/QCMapp/QCMapp-persitence/src/main/java/fr/dawan/formation/QCMappPersistence/SingletonConnection.java
@@ -0,0 +1,34 @@
+package fr.dawan.formation.QCMappPersistence;
+
+
+	import java.io.File;
+import java.io.FileInputStream;
+import java.sql.Connection;
+	import java.sql.DriverManager;
+import java.util.Properties;
+
+	public class SingletonConnection {
+		
+		private static Connection connection;
+		private static Properties props =  new Properties();
+		
+		static {
+			try {
+				props.load(new FileInputStream(new File("app.properties")));
+                String url = props.getProperty("url");
+                String user = props.getProperty("user");
+                String pass = props.getProperty("pass");
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				connection = DriverManager.getConnection(url,user,pass);
+				
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+				// TODO: handle exception
+			}
+			
+		}
+		public static Connection getConnection() {
+			return connection;
+		}
+}

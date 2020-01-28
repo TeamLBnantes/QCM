50fb56b745b3961c92bc47443daa4ce5f0976644
laurent.boureau.portable <laurent.boureau.nantes@gmail.com>
2020-01-29 00:27:04 +0100

    fin implementation DaoAnswer + test DaoAnswer

--- 01Code/QCMapp/QCMapp-persitence/src/main/java/fr/dawan/formation/QCMappPersistenceDAO/DAOAnswer.java
+++ 01Code/QCMapp/QCMapp-persitence/src/main/java/fr/dawan/formation/QCMappPersistenceDAO/DAOAnswer.java
@@ -1,40 +1,117 @@
 package fr.dawan.formation.QCMappPersistenceDAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 import fr.dawan.formation.QCMappModel.Answer;
+import fr.dawan.formation.QCMappPersistence.SingletonConnection;
 import fr.dawan.formation.QCMappPersistenceInterfaces.DAOAnswerInterface;
+import sun.reflect.generics.tree.ReturnType;
 
 public class DAOAnswer implements DAOAnswerInterface{
 
 	@Override
 	public void create(Answer answer) {
-		// TODO Auto-generated method stub
-		
+		Connection cn = SingletonConnection.getConnection();
+		try {
+			String sql = "INSERT INTO Answer(body, expectedAnswer, commentPostAnswer, idQuestion) VALUES(?,?,?,?)";
+			PreparedStatement ps = cn.prepareStatement(sql);
+			ps.setString(1, answer.getBody());
+			ps.setBoolean(2, answer.isExpectedAnswer());
+			ps.setString(3, answer.getCommentPostAnswer());
+			ps.setInt(4, answer.getIdQuestion());
+			ps.executeUpdate();
+			ps.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void update(Answer answer) {
-		// TODO Auto-generated method stub
-		
+		// update de la reponse, pointage depuis answer.id
+		Connection cn = SingletonConnection.getConnection();
+		try {
+			String sql = "UPDATE Answer SET body=?,expectedAnswer=?, commentPostAnswer=?, idQuestion=? WHERE id=?";
+			PreparedStatement ps = cn.prepareStatement(sql);
+			ps.setString(1, answer.getBody());
+			ps.setBoolean(2, answer.isExpectedAnswer());
+			ps.setString(3, answer.getCommentPostAnswer());
+			ps.setInt(4, answer.getIdQuestion());
+			ps.setInt(5, answer.getId());
+			ps.executeUpdate();
+			ps.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void delete(int idAnswer) {
-		// TODO Auto-generated method stub
+		// sup de la reponse dont id=idAnswer
+		Connection cn = SingletonConnection.getConnection();
+		Answer retourAswer=null;
+		try {
+			retourAswer=searchById(idAnswer);
+			if (retourAswer!=null) {
+				String sql = "DELETE FROM Answer  WHERE id=?";
+				PreparedStatement ps = cn.prepareStatement(sql);
+				ps.setInt(1, idAnswer);
+				ps.executeUpdate();
+				ps.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 
 	@Override
-	public ArrayList<Answer> searchById(int idAnswer) {
-		// TODO Auto-generated method stub
-		return null;
+	public Answer searchById(int idAnswer) {
+		// recherche d'une reponse par son id
+		Connection cn = SingletonConnection.getConnection();
+		Answer retourAswer=null;
+		try {
+			String sql = "SELECT * FROM answer WHERE id = ?";
+			PreparedStatement ps = cn.prepareStatement(sql);
+			ps.setInt(1, idAnswer);
+			
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				retourAswer = new Answer(rs.getInt("id"), rs.getString("body"), 
+						rs.getBoolean("expectedAnswer"), rs.getString("commentPostAnswer"), rs.getInt("idQuestion"));
+			}
+			rs.close();
+			ps.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return retourAswer;
 	}
 
 	@Override
 	public ArrayList<Answer> searchByIdQuestion(int idQuestion) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Answer> answers = new ArrayList<Answer>();
+		Connection cn = SingletonConnection.getConnection();
+		try {
+			String sql = "SELECT * FROM answer WHERE idQuestion = ?";
+			PreparedStatement ps = cn.prepareStatement(sql);
+			ps.setInt(1, idQuestion);
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				answers.add(new Answer(rs.getInt("id"), rs.getString("body"), 
+						rs.getBoolean("expectedAnswer"), rs.getString("commentPostAnswer"), rs.getInt("idQuestion")));
+			}
+			ps.close();
+			rs.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return answers;
 	}
 
 }
--- 01Code/QCMapp/QCMapp-persitence/src/main/java/fr/dawan/formation/QCMappPersistenceInterfaces/DAOAnswerInterface.java
+++ 01Code/QCMapp/QCMapp-persitence/src/main/java/fr/dawan/formation/QCMappPersistenceInterfaces/DAOAnswerInterface.java
@@ -10,6 +10,6 @@ public interface DAOAnswerInterface {
 	public void create(Answer answer);
 	public void update(Answer answer);
 	public void delete(int idAnswer);
-	public ArrayList<Answer> searchById (int idAnswer);
+	public Answer searchById (int idAnswer);
 	public ArrayList<Answer> searchByIdQuestion (int idQuestion);
 }
